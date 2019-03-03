c5cc96fde22c58212fe8d77df490b803c49f0380
Dario <salina.dario@gmail.com>
2019-03-03 20:46:32 +0100

    minor adj

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,10 +4,6 @@ import { Link } from "react-router-dom";
 
 export default function Home(props) {
   return (
-    // <Link to={`/signup`}>SignUp</Link>
-    // <Link to={`/login`}>Login</Link>
-    // <Link to={`/events`}>Events</Link>
-    // <Link to={`/logout`}> LogOut</Link>
     <nav class="tc pb3 center">
       <a class="link dim gray b f6 f5-ns dib mr3" href="#" title="Home">
         <Link to={`/signup`}>SignUp</Link>
@@ -20,7 +16,7 @@ export default function Home(props) {
         <Link to={`/events`}>Events</Link>
       </a>
       <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Store">
-        <Link to={`/logout`}> LogOut</Link>
+        <Link to={`/logout`}>LogOut</Link>
       </a>
     </nav>
   );
