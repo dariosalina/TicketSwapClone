b95384ea3edfb835eb5fb956c0ee08961df5d356
Dario <salina.dario@gmail.com>
2019-03-04 20:15:25 +0100

    final adj

--- client/src/components/EventForm.js
+++ client/src/components/EventForm.js
@@ -41,20 +41,20 @@ export default function EventForm(props) {
             />
           </div>
           <div class="mt3">
-            <label class="db fw4 lh-copy f6">Start date(YYYY-DD-MM):</label>
+            <label class="db fw4 lh-copy f6">Start date:</label>
             <input
               class="pa2 input-reset ba bg-transparent w-100 measure"
-              type="text"
+              type="date"
               name="start_date"
               onChange={props.onChange}
               value={props.values.start_date}
             />
           </div>
           <div class="mt3">
-            <label class="db fw4 lh-copy f6">End date(YYYY-DD-MM):</label>
+            <label class="db fw4 lh-copy f6">End date:</label>
             <input
               class="pa2 input-reset ba bg-transparent w-100 measure"
-              type="text"
+              type="date"
               name="end_date"
               onChange={props.onChange}
               value={props.values.end_date}
